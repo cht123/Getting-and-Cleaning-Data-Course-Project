e16770c6708d1eb3a31edbaf9b6a77ee7ae4e179
Michael Taylor <michaelxtaylor@gmail.com>
2015-01-25 10:27:57 -0600

    updated comments for clarity

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,13 @@
 # change to directory
 setwd("~/Classes/DSS/03_Getting_and_Cleaning_Data/Course_Project")
 
-## READ HEADER FILES
+## 01 READ HEADER FILES
 features <- read.table("data/UCI HAR Dataset/features.txt")
 
 activity.labels <- read.table("data/UCI HAR Dataset/activity_labels.txt")
 names(activity.labels) <- c("activity_code", "activity_desc")
 
-## READ TRAINING DATA
+## 02 READ TRAINING DATA
 # read subject column
 subject.train <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
 names(subject.train) <- "Subject"
@@ -24,7 +24,7 @@ names(y.train) <- "activity_code"
 train <- cbind(Treatment = "train", subject.train, y.train, x.train)
 
 
-## READ TEST DATA
+## 03 READ TEST DATA
 # read subject column
 subject.test <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
 names(subject.test) <- "Subject"
@@ -40,20 +40,18 @@ colnames(y.test) <- "activity_code"
 # join columns into single frame
 test <- cbind(Treatment = "test", subject.test, y.test, x.test)
 
-## Append test data to train data
+## 04 Append test data to train data
 all.data <- rbind(test, train)
 
-## add descriptive activity label names
+## 05 add descriptive activity label names
 library(dplyr)
 all.data <- inner_join(all.data, activity.labels)
 
-## keep only avg and mean columns
+## 06 keep only avg and mean columns
 final.data <- cbind(all.data[,c(1,2,565)], all.data[ ,grepl("mean[^Freq]()|std()", names(all.data))])
 
-## Export data
-write.table(final.data, "course_project.txt",row.name=FALSE)
 
-## create summarized data set
+## 07 create and export summarized data set
 summary <- final.data %>%
   group_by(Subject, activity_desc) %>%
   summarise_each(funs(mean), 4:69)
